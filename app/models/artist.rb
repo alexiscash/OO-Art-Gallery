ee176ac0fac9c2e260260bff20396f3f852ab1bb
alexiscash <alexiscash97@rocketmail.com>
2019-09-24 14:34:13 -0500

    test

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,10 +1,56 @@
 class Artist
 
+  @@all = []
+
   attr_reader :name, :years_experience
 
   def initialize(name, years_experience)
     @name = name
     @years_experience = years_experience
+
+    @@all << self
   end
+
+  def self.all
+    @@all
+  end
+
+  def paintings
+    Painting.all.select do |painting|
+      painting.artist == self
+    end
+  end
+
+  def galleries
+    self.paintings.map do |painting|
+      painting.gallery
+    end.uniq
+  end
+
+  def cities
+    self.galleries.map do |gallery|
+      gallery.city
+    end
+  end
+
+  # def self.total_experience
+  #   total_years = 0
+  #   self.all.each do |i|
+  #     total_years += i.years_experience
+  #   end
+  #   total_years
+  # end
+
+  def self.total_experience
+    self.all.map(&:years_experience).inject(&:+)
+  end
+
+  def self.most_prolific
+    self.all.max_by do |i|
+
+    end
+
+  end
+
 
 end
--- app/models/painting.rb
+++ app/models/painting.rb
@@ -1,12 +1,28 @@
 class Painting
 
-  attr_reader :title, :price
-
   @@all = []
 
-  def initialize(title, price)
+  attr_reader :title, :price, :artist, :gallery
+
+  def initialize(title, price, artist, gallery)
     @title = title
     @price = price
+    @artist = artist
+    @gallery = gallery
+
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.total_price
+    total = 0
+    self.all.each do |i|
+      total += i.price
+    end
+    total
   end
 
 end
